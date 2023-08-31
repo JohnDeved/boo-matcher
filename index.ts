2fa5487d07277c4571f79f1bbeb1d31c10b30e2e
Johann Berger <johann@objekt.stream>
2023-08-31 19:54:14 +0200

    ~

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ async function getProfiles (token: string) {
 }
 
 async function sendLike (id: string, token: string) {
-  var likeHeaders = new Headers();
+  const likeHeaders = new Headers();
   likeHeaders.append("authorization", token)
   likeHeaders.append("content-type", "application/json")
   likeHeaders.append("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
