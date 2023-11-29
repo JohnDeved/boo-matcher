9f0fe95af1aedc94131d732aa80b89b926585ad1
undefined <johann@objekt.stream>
2023-11-29 22:39:51 +0100

    ~ cleaner script

--- boo.bash
+++ boo.bash
@@ -10,47 +10,59 @@ if [ -z "$GOOGLE_KEY" ] || [ -z "$GOOGLE_TOKEN" ]; then
   exit 1
 fi
 
+send_request() {
+  local method=$1
+  local url=$2
+  local data=$3
+  local headers=("${@:4}")
+  local header_flags=()
+
+  for header in "${headers[@]}"; do
+    header_flags+=("-H" "$header")
+  done
+
+  echo "curl -s -X $method $header_flags -d \"$data\" $url" >&2
+  response=$(curl -s -X $method "${header_flags[@]}" -d "$data" $url)
+
+  echo $response
+}
+
 get_token() {
   local url="https://securetoken.googleapis.com/v1/token?key=$GOOGLE_KEY"
   local data="grant_type=refresh_token&refresh_token=$GOOGLE_TOKEN"
+  local headers=("Content-Type: application/x-www-form-urlencoded")
 
-  response=$(curl -s -X POST -H 'Content-Type: application/x-www-form-urlencoded' -d $data $url)
-  echo $response | jq -r '.access_token'
+  send_request "POST" $url $data "${headers[@]}" 
 }
 
 get_profiles() {
   local token=$1
   local url="https://api.prod.boo.dating/v1/user/dailyProfiles"
-  local headers=("Authorization: $token" "Content-Type: application/json" "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+  local headers=("Authorization: $token")
 
-  response=$(curl -s -X GET -H "${headers[0]}" -H "${headers[1]}" -H "${headers[2]}" $url)
-  echo $response | jq -c '.profiles[]'
+  send_request "GET" $url "" "${headers[@]}"
 }
 
 send_like() {
   local id=$1
   local token=$2
   local url="https://api.prod.boo.dating/v1/user/sendLike"
-  local headers=("Authorization: $token" "Content-Type: application/json" "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36")
+  local headers=("Authorization: $token" "Content-Type: application/json")
   local data="{\"user\": \"$id\"}"
 
-  response=$(curl -s -w "\n%{http_code}" -X PATCH -H "${headers[0]}" -H "${headers[1]}" -H "${headers[2]}" -d "$data" $url)
-  http_code=$(tail -n1 <<< "$response")
-  body=$(sed '$d' <<< "$response")
-
-  
-  echo "HTTP status code: $http_code"
-  echo "Response body: $body"
+  send_request "PATCH" "$url" "$data" "${headers[@]}"
 }
 
 main() {
-  token=$(get_token)
-  profiles=$(get_profiles $token)
+  token=$(get_token | jq -r '.access_token')
+  profiles=$(get_profiles $token | jq -c '.profiles[]')
+
   while IFS= read -r profile; do
     id=$(echo $profile | jq -r '._id')
     name=$(echo $profile | jq -r '.firstName')
     echo "Liking $name..."
-    send_like $id $token
+    response=$(send_like $id $token)
+    echo "Response: $response"
   done <<< "$profiles"
 }
 
